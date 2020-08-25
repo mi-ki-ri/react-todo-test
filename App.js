4afaffa355451a0e42f3907fe93fe0831c2bce88
mi_ki_ri <mi.ki.ri.hassha.p@gmail.com>
2020-08-26 06:34:28 +0900

    initial

--- App.js
+++ App.js
@@ -1,12 +1,67 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import {
+  StyleSheet,
+  Button,
+  Text,
+  View,
+  SafeAreaView,
+  FlatList,
+  TextInput
+} from "react-native";
+import storage from "./Components/storage";
+
+const Task = ({ name, id }) => {
+
+  const onChangeText = (text, id)=>{
+    // console.log(id)
+    
+    DATA[id].name = text
+    name = text
+  }
+
+  return (
+    <View style={{ alignItems: "center" }}>
+      <TextInput
+        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+        onChangeText={(text) => onChangeText(text, id)}
+        value={name}
+      />
+    </View>
+  );
+};
+
+const Item = function (name, id) {
+  this.name = name;
+  this.id = id;
+};
+
+const DATA = [new Item("TASK1", 0), new Item("Task2", 1)];
+
+const Tasks = (props) => {
+  const renderItem = ({ item }) => <Task name={item.name} id={item.id} />;
+
+  return (
+    <SafeAreaView style={styles.container}>
+      <FlatList
+        data={DATA}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}
+      />
+    </SafeAreaView>
+  );
+};
 
 export default function App() {
+  const addItem = () => {
+    DATA.push(new Item("test", DATA.length));
+  };
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>TaskPallete</Text>
+      <Button title="ADD" onPress={addItem} />
+      <Tasks />
     </View>
   );
 }
@@ -14,8 +69,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
