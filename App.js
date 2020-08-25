f5b56fed4f520ddd6e3260c9eca897fa76f9fef3
mi_ki_ri <mi.ki.ri.hassha.p@gmail.com>
2020-08-26 07:43:18 +0900

    imaicih

--- App.js
+++ App.js
@@ -7,63 +7,94 @@ import {
   View,
   SafeAreaView,
   FlatList,
-  TextInput
+  TextInput,
 } from "react-native";
 import storage from "./Components/storage";
 
-const Task = ({ name, id }) => {
+class Task extends React.Component {
+  constructor(props) {
+    super(props);
 
-  const onChangeText = (text, id)=>{
-    // console.log(id)
-    
-    DATA[id].name = text
-    name = text
   }
+  render() {
+    const onChangeText = (e, text, id) => {
+      console.log(text)
 
-  return (
-    <View style={{ alignItems: "center" }}>
-      <TextInput
-        style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
-        onChangeText={(text) => onChangeText(text, id)}
-        value={name}
-      />
-    </View>
-  );
-};
+      // this.props.data[id].name = text;
+      this.props.name = text;
+      
+    };
+
+    console.log("rennder")
+
+    return (
+      <View style={{ alignItems: "center" }}>
+        <TextInput
+          style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
+          onKeyPress={(text) => onChangeText( text, this.props.id)}
+          value={this.props.name}
+        />
+      </View>
+    );
+  }
+}
 
 const Item = function (name, id) {
   this.name = name;
   this.id = id;
 };
 
-const DATA = [new Item("TASK1", 0), new Item("Task2", 1)];
+class Tasks extends React.Component {
+  
+  render() {
+    console.log("render", this.props.data)
 
-const Tasks = (props) => {
-  const renderItem = ({ item }) => <Task name={item.name} id={item.id} />;
+    const renderItem = ({ item }) => (
+      <Task name={item.name} id={item.id} data={this.props.data} />
+    );
 
-  return (
-    <SafeAreaView style={styles.container}>
-      <FlatList
-        data={DATA}
-        renderItem={renderItem}
-        keyExtractor={(item) => item.id}
-      />
-    </SafeAreaView>
-  );
-};
+    return (
+      <SafeAreaView style={styles.container}>
+        <FlatList
+          data={this.props.data}
+          renderItem={renderItem}
+          keyExtractor={(item) => item.id}
+        />
+      </SafeAreaView>
+    );
+  }
+}
 
-export default function App() {
-  const addItem = () => {
-    DATA.push(new Item("test", DATA.length));
-  };
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
 
-  return (
-    <View style={styles.container}>
-      <Text>TaskPallete</Text>
-      <Button title="ADD" onPress={addItem} />
-      <Tasks />
-    </View>
-  );
+    this.state = { data:[new Item("test", 0)] }
+
+  }
+
+  render() {
+    const addItem = () => {
+      
+      let newArr = this.state.data.concat(new Item("test", this.state.data.length))
+      
+
+      this.setState({data: newArr}, ()=>{
+        console.log(this.state.data)
+
+      })
+
+      
+    };
+
+    return (
+      <View style={styles.container}>
+        <Text>TaskPallete</Text>
+        <Button title="ADD" onPress={addItem} />
+        <Tasks data={this.state.data} />
+      </View>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
